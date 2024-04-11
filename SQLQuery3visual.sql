77f5d1b36f7af26a91a6c1bb6b58752c7f023c57
junior2001-art <142093199+junior2001-art@users.noreply.github.com>
2024-04-11 21:53:34 +0000

    Add files via upload

---
+++ SQLQuery3visual.sql
@@ -0,0 +1,7 @@
+SELECT TOP (1000) [continent]
+      ,[location]
+      ,[date]
+      ,[population_density]
+      ,[new_vaccinations]
+      ,[rolling]
+  FROM [PorfolioProject].[dbo].[percentepopulation]
